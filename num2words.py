ee79fb06ca1e2dff837894c2a1c0c96ebcad1d19
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-05-07 13:24:47 -0300

    initial commit

---
+++ num2words.py
@@ -0,0 +1,8 @@
+class NumToWords:
+  def __init__(self):
+    self.low_numwords = ['zero', 'um', 'dois', 'três', 'quatro', 'cinco',
+      'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze',
+      'catorze', 'quinze', 'dezasseis', 'dezassete', 'dezoito', 'dezanove',
+      'vinte']
+    self.dozens = ['', '', 'vinte', 'trinta', 'quarenta', 'cinquenta',
+      'sessenta', 'setenta', 'oitenta', 'noventa']
