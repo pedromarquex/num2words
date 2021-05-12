1bc76d8e381522b76ddccf6d98f3f9c4080776c2
Pedro Marques <junnior.pedro.pm@gmail.com>
2021-05-12 00:18:44 -0300

    update converter to handle hundreds

--- _tests/test_unit.py
+++ _tests/test_unit.py
@@ -1,25 +1,41 @@
 import unittest
 from num2words.num2words import NumToWords
 
+
 class TestNum2Words(unittest.TestCase):
-	def test_convert(self):
-		converter = NumToWords()
-		self.assertEqual(
-				converter.perform_convert('99,00'),
-				'noventa e nove reais'
-			)
-		self.assertEqual(
-				converter.perform_convert('90,00'),
-				'noventa reais'
-			)
-		self.assertEqual(converter.perform_convert('22,00'), 'vinte e dois reais')
-		self.assertEqual(converter.perform_convert('15,00'), 'quinze reais')
-		self.assertEqual(converter.perform_convert('2,00'), 'dois reais')
-		self.assertEqual(converter.perform_convert('2,99'), 'dois reais e noventa e nove centavos')
-		self.assertEqual(converter.perform_convert('2,90'), 'dois reais e noventa centavos')
-		self.assertEqual(converter.perform_convert('2,20'), 'dois reais e vinte centavos')
-		self.assertEqual(converter.perform_convert('2,15'), 'dois reais e quinze centavos')
-		self.assertEqual(converter.perform_convert('2,02'), 'dois reais e dois centavos')
+    def test_convert(self):
+        converter = NumToWords()
+        self.assertEqual(
+            converter.perform_convert('99,00'),
+            'noventa e nove reais'
+        )
+        self.assertEqual(
+            converter.perform_convert('90,00'),
+            'noventa reais'
+        )
+        self.assertEqual(converter.perform_convert(
+            '22,00'), 'vinte e dois reais')
+        self.assertEqual(converter.perform_convert('15,00'), 'quinze reais')
+        self.assertEqual(converter.perform_convert('2,00'), 'dois reais')
+        self.assertEqual(converter.perform_convert('2,99'),
+                         'dois reais e noventa e nove centavos')
+        self.assertEqual(converter.perform_convert(
+            '2,90'), 'dois reais e noventa centavos')
+        self.assertEqual(converter.perform_convert(
+            '2,20'), 'dois reais e vinte centavos')
+        self.assertEqual(converter.perform_convert(
+            '2,15'), 'dois reais e quinze centavos')
+        self.assertEqual(converter.perform_convert(
+            '2,02'), 'dois reais e dois centavos')
+        self.assertEqual(converter.perform_convert('200,02'),
+                         'duzentos reais e dois centavos')
+        self.assertEqual(converter.perform_convert(
+            '100,02'), 'cem reais e dois centavos')
+        self.assertEqual(converter.perform_convert('120,02'),
+                         'cento e vinte reais e dois centavos')
+        self.assertEqual(converter.perform_convert('121,02'),
+                         'cento e vinte e um reais e dois centavos')
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- num2words/num2words.py
+++ num2words/num2words.py
@@ -1,47 +1,54 @@
 class NumToWords:
-	def __init__(self):
-		self.low_numwords = ['', 'um', 'dois', 'três', 'quatro', 'cinco',
-			'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze',
-			'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove']
-		self.dozens = ['', '', 'vinte', 'trinta', 'quarenta', 'cinquenta',
-			'sessenta', 'setenta', 'oitenta', 'noventa']
-		self.hundreds = ['0', 'cem', 'duzentos', 'trezentos', 'quatrocentos',
-			'quinhentos', 'seissentos', 'setecentos', 'oitocentos',
-			'novecentos']
+    def __init__(self):
+        self.low_numwords = ['', 'um', 'dois', 'três', 'quatro', 'cinco',
+                             'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze',
+                             'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove']
+        self.dozens = ['', '', 'vinte', 'trinta', 'quarenta', 'cinquenta',
+                       'sessenta', 'setenta', 'oitenta', 'noventa']
+        self.hundreds = ['0', 'cento', 'duzentos', 'trezentos', 'quatrocentos',
+                         'quinhentos', 'seissentos', 'setecentos', 'oitocentos',
+                         'novecentos']
 
-	def __get_parts(self, num):
-		integer_part, decimal_part = map(
-			  lambda part : part.strip(),
-			   num.split(','))
-		return integer_part, decimal_part
+    def __get_parts(self, num):
+        integer_part, decimal_part = map(
+            lambda part: part.strip(),
+            num.split(','))
+        return integer_part, decimal_part
 
-	def __convert(self, number):
-		words = []
-		num = int(number)
-		if num == 0:
-			return
-		if num < 1000 and num > 99:
-			pass
-		if num < 100 and num > 19:
-			n = num // 10
-			words.append(self.dozens[n])
-			num = num % 10
-		if num > 0 and num <= 19:
-			words.append('e') if len(words) > 0 else None
-			words.append(self.low_numwords[num])
+    def __convert(self, number):
+        words = []
+        num = int(number)
+        if num == 0:
+            return
+        if num < 1000 and num > 99:
+            if num / 100 == 1 and num % 100 == 0:
+                words.append('cem')
+            else:
+                n = num // 100
+                words.append(self.hundreds[n])
+                num = num % 100
+        if num < 100 and num > 19:
+            n = num // 10
+            words.append('e') if len(words) > 0 else None
+            words.append(self.dozens[n])
+            num = num % 10
+        if num > 0 and num <= 19:
+            words.append('e') if len(words) > 0 else None
+            words.append(self.low_numwords[num])
 
-		return ' '.join(words)
+        return ' '.join(words)
 
-	def perform_convert(self, num):
-		final_words = []
-		integer_part, decimal_part = map(
-			lambda part : int(part),
-			self.__get_parts(num)
-			)
-		integer_returned = self.__convert(integer_part)
-		final_words.append(integer_returned + ' reais') if integer_returned is not None else None
-		decimal_returned = self.__convert(decimal_part)
-		final_words.append('e ' + decimal_returned + ' centavos') if decimal_returned is not None else None
+    def perform_convert(self, num):
+        final_words = []
+        integer_part, decimal_part = map(
+            lambda part: int(part),
+            self.__get_parts(num)
+        )
+        integer_returned = self.__convert(integer_part)
+        final_words.append(integer_returned +
+                           ' reais') if integer_returned is not None else None
+        decimal_returned = self.__convert(decimal_part)
+        final_words.append('e ' + decimal_returned +
+                           ' centavos') if decimal_returned is not None else None
 
-		return ' '.join(final_words)
-
+        return ' '.join(final_words)
